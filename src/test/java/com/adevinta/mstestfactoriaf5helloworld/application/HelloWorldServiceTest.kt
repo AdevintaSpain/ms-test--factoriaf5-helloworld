d3e2f8081be25ea7188dec8650ac8137ed7eac6f
Roger Viñas <roger.vinas@adevinta.com>
2022-02-03 16:00:22 +0100

    Add HelloWorldRepository

--- src/test/java/com/adevinta/mstestfactoriaf5helloworld/application/HelloWorldServiceTest.kt
+++ src/test/java/com/adevinta/mstestfactoriaf5helloworld/application/HelloWorldServiceTest.kt
@@ -1,13 +1,26 @@
 package com.adevinta.mstestfactoriaf5helloworld.application
 
+import com.adevinta.mstestfactoriaf5helloworld.domain.HelloWorldRepository
+import com.nhaarman.mockito_kotlin.doReturn
+import com.nhaarman.mockito_kotlin.mock
+import com.nhaarman.mockito_kotlin.whenever
 import org.assertj.core.api.Assertions.assertThat
 import org.junit.jupiter.api.Test
 
 class HelloWorldServiceTest {
 
   @Test
-  fun `should say hello`() {
-    assertThat(HelloWorldService().hello())
-      .isEqualTo("Hello Coders!!!")
+  fun `should say greeting`() {
+    // given
+    val helloWorldRepository = mock<HelloWorldRepository>()
+    doReturn("Hola").whenever(helloWorldRepository).greeting()
+    val helloWorldService = HelloWorldService(helloWorldRepository)
+
+    // when
+    val helloWorldMessage = helloWorldService.hello()
+
+    // then
+    assertThat(helloWorldMessage)
+      .isEqualTo("Hola Coders!!!")
   }
 }
